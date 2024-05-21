1cc50f09f4356b9bad40424d4d7a722a2f291bf9
Camilla <camilla_s.r@hotmail.com>
2024-05-21 16:01:48 -0300

    feat: algoritmo funcionando

---
+++ contaBanco/src/contaTerminal.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class contaTerminal {
+    public static void main(String[] args) {
+        Scanner lerTexto = new Scanner(System.in);
+        Scanner lerNum = new Scanner(System.in);
+        
+        int numeroConta;
+        String agencia;
+        String nomeCliente;
+        double saldo = 345.22;
+
+        System.out.print("Por favor, informe o número da conta\n");
+        numeroConta = lerNum.nextInt();
+
+        System.out.print("Informe a agência\n");
+        agencia = lerTexto.nextLine();
+
+        System.out.print("Informe seu nome\n");
+        nomeCliente = lerTexto.nextLine();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, "
+        + "sua agência é " + agencia + ", conta " + numeroConta
+        + " e seu saldo " + saldo + " já está disponível para saque");
+    }
+}
